a24137fbbbdfb3cc17a586e7c39e28bc10f413b1
weiaik0602 <ng_weiaik@yahoo.com>
2017-10-23 10:36:41 +0800

    show myapp.exe

---
+++ src/main.c
@@ -0,0 +1,38 @@
+
+
+#include "Traversal.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Node node1={NULL,NULL,1};
+Node node3={NULL,NULL,3};
+Node node7={NULL,NULL,7};
+Node node24={NULL,NULL,24};
+Node node28={NULL,NULL,28};
+Node node2={&node1,&node3,2};
+Node node5={NULL,&node7,5};
+Node node15={NULL,NULL,15};
+Node node25={&node24,&node28,25};
+Node node4={&node2,&node5,4};
+Node node20={&node15,&node25,20};
+Node node10={&node4,&node20,10};
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void main(void)
+{
+    printf("%s","Inorder:\n");
+    printTraversalInorder(&node10);
+
+    printf("%s","printTraversalPostorder:\n");
+    printTraversalPostorder(&node10);
+
+    printf("%s","printTraversalPreorder:\n");
+    printTraversalPreorder(&node10);
+}
